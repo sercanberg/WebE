8ac97eef48c4e9aec1ead415daac5de711c06db1
Leon-Ams <92448233+Leon-Ams@users.noreply.github.com>
2022-05-26 21:07:50 +0200

    API Daten eingebunden (Test mit whereisiss)

--- symfonyProject/src/Controller/Bab.php
+++ symfonyProject/src/Controller/Bab.php
@@ -1,8 +1,14 @@
 <?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+
 class Bab extends AbstractController
 {
     /**
@@ -25,6 +31,19 @@ class Bab extends AbstractController
      */
     public function number1(): Response
     {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+
+
         $articles = [
             '0' => ['title' => 'Frühling', 'body' => 'Der Frühling beginnt ...'],
             '1' => ['title' => 'Sommer', 'body' => 'Der Sommert ist ...'],
@@ -32,7 +51,8 @@ class Bab extends AbstractController
             '3' => ['title' => 'Winter', 'body' => 'Der Winter war...']
         ];
         return $this->render('bab/herren.html.twig', [
-            'articles' => $articles
+            'articles' => $articles,
+            'response' => $content
         ]);
     }
     /**
