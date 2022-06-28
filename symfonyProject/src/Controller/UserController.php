04c46bf92a03a12fa99b8d428c311eeccaf74b9e
Leon-Ams <92448233+Leon-Ams@users.noreply.github.com>
2022-06-28 13:56:26 +0200

    update account

--- symfonyProject/src/Controller/UserController.php
+++ symfonyProject/src/Controller/UserController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller;
+use PhpParser\Node\Expr\Array_;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Persistence\ManagerRegistry;
@@ -29,9 +31,41 @@ class UserController extends AbstractController
             [
                 'username' => $username
             ]);
+        if ($current_user == null){
+            $current_user = array();
+            $current_user["0"] = "n/a";
+        }
+        return $this->render('bab/account.html.twig', [
+            'username' => $username,
+            'user' => $current_user["0"]
+        ]);
+    }
+    /**
+     * @Route("/account-set")
+     */
+    public function acc_set(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $content = $request -> getContent();
+        $split = explode("=", $content);
+        $city = $split[2];
+        $street = explode("&", $split[1])[0];
+        print_r($city);
+        print_r($street);
+        $username = $this->get_user();
+
+        $current_user= $doctrine->getRepository(User::class)->findBy(
+            [
+                'username' => $username
+            ]);
+        if ($current_user == null){
+            $current_user = array();
+            $current_user["0"] = "n/a";
+        }
 
         return $this->render('bab/account.html.twig', [
-            'username' => $current_user
+            'username' => $username,
+            'user' => $current_user["0"]
+
         ]);
     }
 }
